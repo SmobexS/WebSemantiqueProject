8b44a2c75c9386d5a195dff8093814d59e79df15
Iness-BouabiD <iness.bouabid2017@gmail.com>
2024-01-06 14:57:52 +0100

    erreur recupération position avec sparql

--- RdfToSparql.py
+++ RdfToSparql.py
@@ -65,56 +65,51 @@ def generate_search_query(date, time):
     return search_query
 
 
-# Problème cas choix par place : latitude = None et longitude = None ( fix: recuperer adresses automatiquement)
+
 def search_by_place(day, time, latitude=None, longitude=None, place=None, max_distance=None):
-    search_query = "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n"
-    search_query += "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"
-    search_query += "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
-    search_query += "PREFIX pwp:<https://ProjectW9s.com/predicate/>\n"
-    search_query += "PREFIX pwo:<https://ProjectW9s.com/object/>\n"
-    search_query += "PREFIX pws:<https://ProjectW9s.com/subject/>\n"
-    search_query += "PREFIX schema: <http://schema.org/>\n"
-    search_query += "PREFIX geof: <http://www.opengis.net/ont/geosparql#>\n"
-    search_query += "SELECT ?restaurant ?name ?openingTime ?closingTime ?address\n"
-    search_query += "WHERE {\n"
-    search_query += "?restaurant a schema:Restaurant ;\n"
-    search_query += "schema:name ?name;\n"
-    search_query += "schema:address ?address_link;\n"
-    search_query += "schema:openingHoursSpecification [\n"
-    search_query += "schema:opens ?openingTime ;\n"
-    search_query += "schema:closes ?closingTime ;\n"
-    search_query += "schema:dayOfWeek ?dayOfWeek\n"
-    search_query += "] .\n"
-    search_query += "?address_link a schema:PostalAddress;\n"
-    search_query += "schema:streetAddress ?address.\n"
+    search_query = (
+        "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n"
+        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"
+        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
+        "PREFIX pwp:<https://ProjectW9s.com/predicate/>\n"
+        "PREFIX pwo:<https://ProjectW9s.com/object/>\n"
+        "PREFIX pws:<https://ProjectW9s.com/subject/>\n"
+        "PREFIX schema: <http://schema.org/>\n"
+        "PREFIX geof: <http://www.opengis.net/ont/geosparql#>\n"
+        "SELECT ?restaurant ?name ?openingTime ?closingTime ?address\n"
+        "WHERE {\n"
+        "?restaurant a schema:Restaurant ;\n"
+        "schema:name ?name;\n"
+        "schema:address ?address_link;\n"
+        "schema:openingHoursSpecification [\n"
+        "schema:opens ?openingTime ;\n"
+        "schema:closes ?closingTime ;\n"
+        "schema:dayOfWeek ?dayOfWeek\n"
+        "] .\n"
+        "?address_link a schema:PostalAddress;\n"
+        "schema:streetAddress ?address.\n"
+    )
 
-
-    search_query += f"FILTER (?dayOfWeek = \"{day}\" && ?openingTime <= \"{time}\" && ?closingTime > \"{time}\")\n"
-
-    if place and max_distance:
-        # Get latitude and longitude for the specified place
+    if place:
         place_latitude, place_longitude = get_lat_long_for_place(place)
-
         if place_latitude is not None and place_longitude is not None:
             search_query += (
-                f"FILTER EXISTS {{ ?restaurant schema:location ?location ."
-                f" ?location schema:address ?restaurantAddress ."
-                f" SERVICE <http://www.openlinksw.com/schemas/virtrdf#> {{"
-                f"   ?restaurantAddress <http://www.w3.org/2003/01/geo/wgs84_pos#lat> {place_latitude} ."
-                f"   ?restaurantAddress <http://www.w3.org/2003/01/geo/wgs84_pos#long> {place_longitude} ."
-                f"   FILTER (geof:distance(?location, geof:point({place_longitude} {place_latitude})) <= {max_distance})"
-                f"}}\n"
+                f"FILTER EXISTS {{"
+                f"  ?restaurant schema:location ?location ."
+                f"  ?location schema:address ?restaurantAddress ."
+                f"  FILTER (geof:distance(?location, geof:point({place_longitude} {place_latitude}), {max_distance}))"
+                f"}} ;\n"
+                f"FILTER (?dayOfWeek = \"{day}\" && ?openingTime <= \"{time}\" && ?closingTime > \"{time}\")\n"
             )
         else:
             print(f"Unable to get coordinates for the place: {place}")
-
+    else:
+        print("No place specified for the search.")
 
     print("Generated SPARQL Query:")
-    print(search_query)  # Add this line to print the generated query
-
+    print(search_query)
 
     return search_query
-
 
 
 def get_lat_long_for_place(place):
