d218eb8a70d7934f6334d4a8fd371970c44eb7c4
SmobexS <saad20010@live.fr>
2024-01-10 00:12:47 +0100

    Final push

--- Collect.py
+++ Collect.py
@@ -12,31 +12,41 @@ def collect(url='https://coopcycle.org/coopcycle.json?_=1704296951899'):
 
     if len(graph) == 0:
 
-        graph = json2rdf(url)
-        json_ld = JsonLDScraper(url)
-        graph = jsonld_to_graph(json_ld, graph)
+        new_dataset()
 
-        insert_query = generate_insert_query(graph)
-        graph = insert_data(insert_query)
-
-        graph = graph.serialize(destination="combined_rdf.txt" ,format='turtle')
-        
+        return None
+    
     else:
 
-        graph = json2rdf(url)
-        json_ld = JsonLDScraper(url)
-        graph = jsonld_to_graph(json_ld, graph)
+        thread = threading.Thread(target=update_dataset)
         
-        insert_query = generate_insert_query(graph)
+        return thread
 
-        delete_data()
-
-        graph = insert_data(insert_query)
-
-        graph = graph.serialize(destination="combined_rdf.txt" ,format='turtle')
         
 
+def new_dataset (url='https://coopcycle.org/coopcycle.json?_=1704296951899'):
 
+    graph = json2rdf(url)
+    json_ld = JsonLDScraper(url)
+    graph = jsonld_to_graph(json_ld, graph)
 
-thread = threading.Thread(target=collect)
+    insert_query = generate_insert_query(graph)
+    graph = insert_data(insert_query)
+
+    graph = graph.serialize(destination="combined_rdf.txt" ,format='turtle')
+
+def update_dataset (url='https://coopcycle.org/coopcycle.json?_=1704296951899'):
+
+    graph = json2rdf(url)
+    json_ld = JsonLDScraper(url)
+    graph = jsonld_to_graph(json_ld, graph)
+        
+    insert_query = generate_insert_query(graph)
+
+    delete_data()
+
+    graph = insert_data(insert_query)
+
+    graph = graph.serialize(destination="combined_rdf.txt" ,format='turtle')
+
     
--- Main.py
+++ Main.py
@@ -1,33 +1,37 @@
 import sys
-import Collect
+from Collect import *
 from Describe import describe
 
 
 def main ():
     
+    thread = collect()
+    if thread != None :
+        
+        thread.start()
 
-    Collect.thread.start()
-
-    describe()
+        describe()
 
 
-    if Collect.thread.is_alive() :
+        if thread.is_alive() :
 
-        validate = False
-        while validate == False :
-            des=input("The collect dataset programme is still running do you want to end it and exit the application (y/n): ")
-            if des =="y":
-                validate = True
-                sys.exit(0)
+            validate = False
+            while validate == False :
+                des=input("The collect dataset programme is still running do you want to end it and exit the application (y/n): ")
+                if des =="y":
+                    validate = True
+                    sys.exit(0)
+                    
+                elif des == "n":
+                    validate = True
                 
-            elif des == "n":
-                validate = True
-            
-            else:
-                validate = False
-                print("Please chose 'y' for yes or 'n' for no.")
+                else:
+                    validate = False
+                    print("Please chose 'y' for yes or 'n' for no.")
     
-
+    else:
+        
+        describe()
 
 if __name__ == "__main__":
     main()
